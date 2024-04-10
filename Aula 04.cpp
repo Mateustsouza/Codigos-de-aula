0f7d1c1f366a54e96bfd43b718f1915e21495224
Mateus Torres <164523965+Mateustsouza@users.noreply.github.com>
2024-04-10 15:31:53 -0300

    Add files via upload

---
+++ Aula 04.cpp
@@ -0,0 +1,81 @@
+/*
+10/04/2024
+
+Aula 04
+
+
+
+*LAÇOS DE REPETIÇÃO
+  
+ *WHILE
+ 
+ while (<condição>){    CONDIÇÃO DE ENTRADA NO LAÇO
+ 
+ comandos1;
+ comandos1;
+ 
+ }
+ 
+ SENTINELA E CONTADOR SÃO AS VARIÁVEIS QUE IRÃO ME PERMITIR SAIR DO LAÇO.
+ 
+ CONTADOR - LOOP DE FIM DETERMINADO - MAIS ADEQUADO O FOR
+ SENTINELA - LOOP DE FIM INDETERMINADO - MAIS ADEQUADO O WIHLE
+ 
+ 
+ 
+ * FOR (inicialização;condição;incremento){]
+ comando1;
+ comando2;
+ 
+ }
+ 
+ 
+for e while são equivalentes
+ 
+ 
+por exemplo: 
+ 
+for(cont=0;cont<10;cont++){
+ }
+ 
+ 
+ cont=0;
+ while(cont<10){
+ 
+ cont++;
+ }
+ 
+ 
+ for (i = 0; i < n; i++){
+cout << "*";
+cout << "\n";
+
+if(i==5){
+
+break;   INTERROMPE EM i =5
+
+}            
+
+
+if(i==5){
+
+continue;   IGNORA O i =5 OU SEJA, VAI FAZER i = 0, 1,2,3,4,6,7,8.....
+
+}            
+
+
+
+
+}
+ 
+ 
+ 
+*/
+
+
+
+
+ 
+#include <iostream>
+#include <cmath> 
+using namespace std;
