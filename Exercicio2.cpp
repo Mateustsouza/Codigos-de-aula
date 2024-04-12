2b6194cdb5587d5bca345539557802cb81b1e094
Mateus Torres <164523965+Mateustsouza@users.noreply.github.com>
2024-04-12 15:23:41 -0300

    Add files via upload

---
+++ Exercicio2.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+
+int main(){
+	
+	int i;
+	int c;
+	
+	for(c=1;c<11;c++){
+	
+	  for(i=0;i<c;i++){
+	  cout << "*";	
+	  }
+   cout << "\n";
+}
+	cout << "\n";
+	
+	for(c=1;c<11;c++){
+	
+	  for(i=0;i<(11-c);i++){
+	  cout << "*";	
+	  }
+  cout << "\n";
+}
+	cout << "\n";
+	
+	for(c=1;c<11;c++){
+	
+	  for(i=0;i<(11-c);i++){
+	  cout << "*";	
+	  }
+  cout << "\n";
+}
+	
+	
+	return 0;
+	
+}
+
+
+
+
+
+
+
+
+
