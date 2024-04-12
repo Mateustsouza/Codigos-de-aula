12ce27a601a8de7d7c91f84ff682009e3e848be5
Mateus Torres <164523965+Mateustsouza@users.noreply.github.com>
2024-04-12 12:09:08 -0300

    Add files via upload

---
+++ TESTE.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;    \
+
+int main(){
+/*    
+//double e =0;              // Já estou incluindo o primeiro termo da soma
+int c;
+int fatorial;
+int i;
+int soma =0;
+
+for(c=2; c<6; c++){               // "c++" = "c+=1"
+
+if (c>1){
+    fatorial = c;    // Atribuindo o valor de c a fatorial
+    
+    for(i=1; i<c; i++){
+        
+        fatorial *= (c-i);
+        
+    }     
+  soma += inv(fatorial);
+} 
+
+else {
+    fatorial = 1;
+
+}
+*/
+
+long double x;
+x=1/3;
+
+
+//}
+//cout << setprecision(3) << fixed;
+cout << "x =  " << x << endl;
+
+return 0;
+}
