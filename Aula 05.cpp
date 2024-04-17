0c6d9e5f4caa3446c2838eaaf3794ada53688544
Mateus Torres <164523965+Mateustsouza@users.noreply.github.com>
2024-04-17 15:30:17 -0300

    Add files via upload

---
+++ Aula 05.cpp
@@ -0,0 +1,71 @@
+/*
+
+Aula Funções
+Data: 17/04/2024
+
+*/
+
+// Não é recomendado que uma função faça mais de uma tarefa. É recomendado que ela faça uma função específica.
+// Vantagem: Reutilização do código.
+
+
+// tipo nome (par1,par2,...) -  o tipo da função segue o tipo da saída.
+// Qunado uma função não retrona nada ela começa com void.
+// SÃO PASSADAS CÓPIAS DO VALOR DAS VARIÁVEIS DE ENTRADA PARA A FUNÇÃO, OU SEJA, A FUNÇÃO NÃO ALTERA O VALOR DAS VARIÁVIS D ENTRADA. EXCETO SE AS VARIÁVEIS SEJAM PASSADAS POR REFERÊNCIA.
+// VETORES SÃO PASSADOS POR REFERÊNCIA. 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
