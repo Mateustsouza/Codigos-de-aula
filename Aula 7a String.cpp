c689bf39699ae271ccdacccf064f8c2c9240b2a3
Mateus Torres <164523965+Mateustsouza@users.noreply.github.com>
2024-06-07 15:30:40 -0300

    Add files via upload

---
+++ Aula 7a String.cpp
@@ -0,0 +1,14 @@
+
+
+
+/*
+
+Toda string é um vetor de caracteres
+
+vOCE PODE INVADIR O ENDEREÇO DE MEMÓRIA DE UMA VARIÁVEL SE VOCE ACESSAR UM INDICE MAIOR QUE O TAMANHO DO VETOR.
+
+s1.size() = s1.length()
+
+
+
+*/
